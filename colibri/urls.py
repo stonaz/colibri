45aa902ac5b78658c5b358dbf69da83cc5402e40
stonaz <stefano.tonazzi@gmail.com>
2013-12-09 10:17:18 +0100

    listview

--- colibri/urls.py
+++ colibri/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from books.views import BookList
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'colibri.views.home', name='home'),
+    url(r'^books/$', BookList.as_view()),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
