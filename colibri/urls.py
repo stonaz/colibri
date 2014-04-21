15964fc88b7df042a191ce425b1657b0e1b7b4cc
stefano <stefano.tonazzi@gmail.com>
2014-04-22 01:03:43 +0200

    Url for Book detail view

--- colibri/urls.py
+++ colibri/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -24,16 +24,19 @@ urlpatterns = patterns('',
     url(r'^update_profile/$', views.update_profile, name='update_profile'),
     
     url(r'^api/v1/books/$', views.book_list ,name='book_list'),
+    url(r'^api/v1/books/(?P<pk>[-\d\.]+)/$', views.book_detail ,name='book_detail'),
     url(r'^api/v1/(?P<user>[-\w\.]+)/books/$', views.user_book_list ,name='user_book_list'),
     url(r'^api/v1/(?P<user>[-\w]+)/holding_books/$', views.user_holding_book_list ,name='user_holding_book_list'),
     url(r'^api/v1/user_profile/$', views.user_profile_list ,name='user_profile_list'),
     url(r'^api/v1/(?P<user>[-\w]+)/user_profile/$', views.user_profile_list ,name='user_profile_list'),
     url(r'^admin/', include(admin.site.urls)),
-)
+)  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-        urlpatterns += patterns(
-                'django.views.static',
-                (r'media/(?P<path>.*)',
-                'serve',
-                {'document_root': settings.MEDIA_ROOT}), )
+#if settings.DEBUG:
+#       urlpatterns += patterns(
+#                'django.views.static',
+#                (r'media/(?P<path>.*)',
+#                'serve',
+#                {'document_root': settings.MEDIA_ROOT}), )
+
+
