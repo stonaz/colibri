b4c649d0046534c6331857db59ab91faca5d4ae9
stonaz <stefano.tonazzi@gmail.com>
2014-06-17 10:25:42 +0200

    Removed Static files

--- static/assets/js/apps/about/show/show_controller.js
+++
@@ -1,8 +0,0 @@
-ColibriApp.module("AboutApp.Show", function(Show, ColibriApp, Backbone, Marionette, $, _){
-  Show.Controller = {
-    showAbout: function(){
-      var view = new Show.Message();
-      ColibriApp.mainRegion.show(view);
-    }
-  };
-});
--- static/assets/js/apps/about/show/show_view.js
+++
@@ -1,5 +0,0 @@
-ColibriApp.module("AboutApp.Show", function(Show, ColibriApp, Backbone, Marionette, $, _){
-  Show.Message = Marionette.ItemView.extend({
-    template: "#about-message"
-  });
-});
