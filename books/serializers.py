0a84f5f747d1eb39192a665aae28a365cea0dd04
stefano <stefano.tonazzi@gmail.com>
2017-11-09 17:16:40 +0100

    book serializer bug

--- books/serializers.py
+++ books/serializers.py
@@ -9,13 +9,12 @@ class BookListSerializer(serializers.ModelSerializer):
     Book list
     """
     owner_username = serializers.CharField(source='owner.username',read_only=True)
-    where_is = serializers.CharField(source='where_is.user',read_only=True)
     
     class Meta:
         model = Book
 
         fields= (
-           'id','author', 'title', 'owner', 'owner_username','comment','note', 'where_is', 'created', 'modified',
+           'id','author', 'title', 'owner', 'owner_username','comment','note', 'created', 'modified',
             )
 
       
@@ -24,12 +23,11 @@ class BookDetailSerializer(serializers.ModelSerializer):
     Book details
     """
     owner_username = serializers.CharField(source='owner.username',read_only=True)
-    where_is = serializers.CharField(source='where_is.user',read_only=True)
     
     class Meta:
         model = Book
         
         fields= (
-           'id','author', 'title', 'owner', 'owner_username','comment','note', 'where_is', 'created', 'modified',
+           'id','author', 'title', 'owner', 'owner_username','comment','note', 'created', 'modified',
             )
 
